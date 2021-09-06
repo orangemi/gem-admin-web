6a0b28fda27fb4c0674a71beb2f2cd64c89f212d
Orange Mi <orangemiwj@gmail.com>
2021-09-07 01:28:46 +0800

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template lang='pug'>
-  .home
-    img(alt='Vue logo' src='../assets/logo.png')
-    hello-world(msg='Welcome to Your Vue.js App')
+  .home Home
+    //- img(alt='Vue logo' src='../assets/logo.png')
 </template>
 
 <script lang='ts'>
